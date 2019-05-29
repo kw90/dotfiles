01fbd84646012b9c06b0cec9402ab428d6b5dbe3
Kai Waelti <Kai.Waelti@dfki.de>
2019-05-29 10:48:51 +0200

    Add i3 debian single monitor config and install

--- i3/install.debian.sh
+++ i3/install.debian.sh
@@ -7,10 +7,10 @@ echo_info "Installing deps"
 . ./install_betterlock.sh
 
 curr_wd=$(pwd)
-echo_info "Sym. linking i3 config file from $curr_wd to ~/.i3/config"
+echo_info "Sym. linking i3 config file from $curr_wd to ~/.config/i3/config"
 
 mkdir -p ~/.i3
-ln -sfT ~/source/dotfiles/i3/config ~/.i3/config
+ln -sfT ~/source/dotfiles/i3/config.debian.wide-display ~/.config/i3/config
 ln -sfT ~/source/dotfiles/i3/i3exit ~/.i3/i3exit
 
 echo_done "i3 config applied!"
