ea7f08a9f0943203c5041824c2c92de8f16f6417
Kai Waelti <kai.waelti@gmail.com>
2019-06-05 13:59:51 +0200

    Add arch wide display config

--- i3/install.sh
+++ i3/install.sh
@@ -10,8 +10,8 @@ curr_wd=$(pwd)
 echo_info "Sym. linking i3 config file from $curr_wd to ~/.i3/config"
 
 mkdir -p ~/.i3
-ln -sfT ~/source/dotfiles/i3/config ~/.i3/config
-ln -sfT ~/source/dotfiles/i3/i3exit ~/.i3/i3exit
+ln -sfT ~/source/dotfiles/i3/config.arch.wide-display ~/.config/i3/config
+ln -sfT ~/source/dotfiles/i3/i3exit ~/.config/i3/i3exit
 
 echo_done "i3 config applied!"
 
