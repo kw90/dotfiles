df38aaa88c18e1fb53273f54c378df33c9514d71
Kai Waelti <kai.waelti@pm.me>
2019-06-23 02:51:28 +0200

    Fix config for multi display

--- polybar/launch-polybar.sh
+++ polybar/launch-polybar.sh
@@ -8,6 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo "screen $m"
     MONITOR=$m polybar top -r -c ~/.config/polybar/polybar.conf &
     MONITOR=$m polybar bottom -r -c ~/.config/polybar/polybar.conf &
   done
