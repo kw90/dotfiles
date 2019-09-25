5762d683c853bbabff96d6170aad309e58e028be
Kai Waelti <kai.waelti@dfki.de>
2019-09-25 12:05:18 +0200

    Add zsh and kitty config

---
+++ zsh/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. ../helpers.sh
+
+echo_info "Installing oh-my-zsh"
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo_info "Installing Spaceship prompt"
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "/home/kw/.oh-my-zsh/custom/themes/spaceship-prompt"
+
+ln -s "/home/kw/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/home/kw/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
+rm ~/.zshrc
+ln -s ~/source/dotfiles/zsh/zshrc ~/.zshrc
+
+. ./packages.sh
+
+echo_done "zshrc config applied"
---
+++ zsh/packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check https://github.com/eosrei/twemoji-color-font
+. ../helpers.sh
+
+echo_info "Installing deps and emojis"
+
+sudo apt-add-repository ppa:eosrei/fonts
+sudo apt-get update
+sudo apt-get install fonts-twemoji-svginot git-flow
+
+echo_done "Emojis successfully applied 🚀"
+
+echo_info "Installing zsh related deps"
+
+pip3 install thefuck
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/bobthecow/git-flow-completion ~/.oh-my-zsh/custom/plugins/git-flow-completion
