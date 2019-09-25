f11e7736c3462f4671c677aa8984a1dfbc0a0bfd
Kai Waelti <kai.waelti@dfki.de>
2019-09-25 12:20:42 +0200

    Add grip

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -13,7 +13,7 @@ echo_done "Emojis successfully applied 🚀"
 
 echo_info "Installing zsh related deps"
 
-pip3 install thefuck
+pip3 install thefuck grip
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/bobthecow/git-flow-completion ~/.oh-my-zsh/custom/plugins/git-flow-completion
