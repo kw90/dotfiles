6e63388e4f6f36f37a020e054a8a16eb18a79b97
Kai Waelti <kai.waelti@pm.me>
2020-05-15 18:59:58 +0200

    Add moc, tilda config and adapt kitty zsh completions

--- tilda/install.arch.sh
+++ tilda/install.arch.sh
@@ -11,5 +11,7 @@ echo_info "Sym. linking tilda config file from $curr_wd to ~/.config/tilda"
 
 mkdir -p ~/.config/tilda
 ln -sfT ~/source/dotfiles/tilda/config_0 ~/.config/tilda/config_0
+ln -sfT ~/source/dotfiles/tilda/config_1 ~/.config/tilda/config_1
+ln -sfT ~/source/dotfiles/tilda/config_2 ~/.config/tilda/config_2
 
 echo_done "tilda config applied!"
