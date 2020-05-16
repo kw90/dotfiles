e10cf9eac0a32781428adaa501752ac50decee9b
Kai Waelti <kai.waelti@pm.me>
2020-05-16 18:52:06 +0200

    Switch to compton-tryzen to get blur method

--- compton/install.arch.sh
+++ compton/install.arch.sh
@@ -2,6 +2,9 @@
 
 . ../helpers.sh
 
+echo_info "Installing compton-tryone-git for blur support"
+yaourt -S compton-tryone-git
+
 curr_wd=$(pwd)
 echo_info "Sym. linking compton config file from $curr_wd to ~/.config/compton"
 
