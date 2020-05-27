4bc1674cf3f059e79c0df7fc98f0195909e383a0
Kai Waelti <kai.waelti@pm.me>
2020-05-27 18:21:45 +0200

    Add mac install and config

---
+++ zsh/packages.mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check https://github.com/eosrei/twemoji-color-font
+. ../helpers.sh
+
+echo_info "Installing deps and emojis"
+
+brew install git-flow-avh
+
+echo_done "Emojis already applied 🚀"
+
+echo_info "Installing zsh related deps"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/bobthecow/git-flow-completion ~/.oh-my-zsh/custom/plugins/git-flow-completion
