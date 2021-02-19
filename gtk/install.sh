1a45f0885b271bd60e966ad85abcdfa41c5375ad
Kai Waelti <kai.waelti@hslu.ch>
2021-02-19 11:05:56 +0100

    fix: gtk linking and settings

--- gtk/install.sh
+++ gtk/install.sh
@@ -7,7 +7,8 @@ yaourt -S bibata-cursor-translucent
 
 echo_info "Sym. linking cursor files and copying config file"
 
-ln -sfT ./index.theme ~/.icons/default/index.theme
+ln -sfT ~/source/dotfiles/gtk/settings.ini ~/.config/gtk-4.0/settings.ini
+ln -sfT ~/source/dotfiles/gtk/index.theme ~/.icons/default/index.theme
 ln -sfT /usr/share/icons/Bibata_Spirit/cursors ~/.icons/default/cursors
 
 echo_done "GTK and cursor config applied!"
